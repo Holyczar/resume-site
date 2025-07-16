57d7197acf2625e128a06beb768833161337736b
Nikita <chainsawx03@gmail.com>
2025-07-16 21:20:46 +0500

    beta 1.2

--- app/script.js
+++ app/script.js
@@ -9,16 +9,6 @@
                 }, index * 200);
             });
             
-            // Анимация прогресс-баров
-            const skillBars = document.querySelectorAll('.skill-progress');
-            skillBars.forEach(bar => {
-                const width = bar.style.width;
-                bar.style.width = '0';
-                setTimeout(() => {
-                    bar.style.width = width;
-                }, 500);
-            });
-        });
         
         
       
